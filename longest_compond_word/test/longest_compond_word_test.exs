b61b1220cf70ef2eb096db87d0c81c1b9e5f0331
Sandesh Soni <sandeshsoni1@gmail.com>
2021-08-18 22:11:23 +0530

    add a test we need

--- longest_compond_word/test/longest_compond_word_test.exs
+++ longest_compond_word/test/longest_compond_word_test.exs
@@ -2,7 +2,13 @@ defmodule LongestCompondWordTest do
   use ExUnit.Case
   doctest LongestCompondWord
 
-  test "greets the world" do
-    assert LongestCompondWord.hello() == :world
+  setup do
+    input = ~w(cat cats catsdogcats catxdogcatsrat dog dogcatsdog hippopotamuses rat ratcat ratcatdog ratcatdogcat)
+    answer = "ratcatdogcat"
+    {:ok, input: input, answer: answer}
+  end
+
+  test "greets the world", state do
+    assert LongestCompondWord.process(state[:input]) == state[:answer]
   end
 end
