8eb7e93b9ad366db9057f07ae5a45b957b7eff5a
Sandesh Soni <sandeshsoni1@gmail.com>
2021-08-18 22:33:34 +0530

    iterate through each word and each letter, and print each character.

--- longest_compond_word/lib/longest_compond_word.ex
+++ longest_compond_word/lib/longest_compond_word.ex
@@ -4,15 +4,38 @@ defmodule LongestCompondWord do
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> LongestCompondWord.hello()
-      :world
-
+  get a list on words.
+  return the longest compound
   """
-  def hello do
-    :world
+  def process(input_words) do
+    largest_word = hd(input_words)
+    for word <- input_words do
+      print(word)
+    end
+
+    largest_word
   end
+
+  def print(word) do
+    parse_letters(word)
+  end
+
+  defp parse_letters("") do
+    print_letter("")
+  end
+
+  defp parse_letters(word) do
+    <<letter, remaining::binary>> = word
+    print_letter(letter)
+    parse_letters(remaining)
+  end
+
+  def print_letter("") do
+    IO.puts("--------------------------------------------------------")
+  end
+
+  def print_letter(letter) do
+    IO.puts("- #{letter} -")
+  end
+
 end
