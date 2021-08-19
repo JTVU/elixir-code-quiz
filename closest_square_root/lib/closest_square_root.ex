c17c2c98c37207ce6d6abe620259951ccfefc001
Sandesh Soni <sandeshsoni1@gmail.com>
2021-08-19 15:26:16 +0530

    init closest square root with test case added

---
+++ closest_square_root/lib/closest_square_root.ex
@@ -0,0 +1,12 @@
+defmodule ClosestSquareRoot do
+  @moduledoc """
+  Documentation for `ClosestSquareRoot`.
+  """
+
+  @doc """
+    find square-root or the nearest square-root
+  """
+  def process(number) do
+    number
+  end
+end
